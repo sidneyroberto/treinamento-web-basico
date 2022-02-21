1c29a87c084d0fac5bbcece5d2ca2915587f688a
Sidney Sousa <sidney.sousa@ifms.edu.br>
2022-02-21 15:38:13 -0400

    incrementando o exemplo02

--- exemplo02/js/index.js
+++ exemplo02/js/index.js
@@ -1,23 +1,42 @@
-/**
- * Cria um alias para document.querySelector
- */
+// Cria um alias para document.querySelector
 const $ = document.querySelector.bind(document)
 
-/**
- * Arrow Function
- */
-const calculate = () => {
-  let value1 = $('#number1').value
-  let value2 = $('#number2').value
-  value1 = !value1 ? '0' : value1
-  value2 = !value2 ? '0' : value2
-  const number1 = parseFloat(value1)
-  const number2 = parseFloat(value2)
-
-  let result = 0
+// Arrow function
+const changeSignal = () => {
   const isSumChecked = $('#sum').checked
   const isSubtractionChecked = $('#subtraction').checked
   const isMultiplicationChecked = $('#multiplication').checked
+
+  let operatorSignal = '/'
+  if (isSumChecked) {
+    operatorSignal = '+'
+  } else if (isSubtractionChecked) {
+    operatorSignal = '-'
+  } else if (isMultiplicationChecked) {
+    operatorSignal = '*'
+  }
+
+  $('#operator-signal').innerText = operatorSignal
+
+  // Invoca a função calculate()
+  calculate()
+}
+
+const calculate = () => {
+  let value1 = $('#number1').value
+  let value2 = $('#number2').value
+
+  // Utiliza o operador ternário para atribuir o valor correto às variáveis
+  value1 = !value1 ? '0' : value1
+  value2 = !value2 ? '0' : value2
+
+  const number1 = parseFloat(value1)
+  const number2 = parseFloat(value2)
+
+  const isSumChecked = $('#sum').checked
+  const isSubtractionChecked = $('#subtraction').checked
+  const isMultiplicationChecked = $('#multiplication').checked
+  let result = 0
 
   if (isSumChecked) {
     result = number1 + number2
@@ -25,27 +44,11 @@ const calculate = () => {
     result = number1 - number2
   } else if (isMultiplicationChecked) {
     result = number1 * number2
-  } else if (number2 > 0) {
+  } else if (number2 !== 0) {
     result = number1 / number2
+  } else {
+    result = 'Não pode dividir por zero'
   }
 
   $('#result').innerText = result
-}
-
-const changeSignal = () => {
-  const isSumChecked = $('#sum').checked
-  const isSubtractionChecked = $('#subtraction').checked
-  const isMultiplicationChecked = $('#multiplication').checked
-
-  let operatorSignal = $('#division').value
-  if (isSumChecked) {
-    operatorSignal = $('#sum').value
-  } else if (isSubtractionChecked) {
-    operatorSignal = $('#subtraction').value
-  } else if (isMultiplicationChecked) {
-    operatorSignal = $('#multiplication').value
-  }
-
-  $('#operator-signal').innerText = operatorSignal
-  calculate()
 }
